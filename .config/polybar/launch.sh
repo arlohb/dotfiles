e74309a31f9b30d629e23fe26bb16c4242f7f1c6
arlohb <arlo@urwick.co.uk>
2022-08-15 11:39:24 +0100

    Removed polybar lol

--- .config/polybar/launch.sh
+++
@@ -1,9 +0,0 @@
-killall polybar
-
-if type "xrandr"; then
-  for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload > /dev/null 2>&1 &
-  done
-else
-  polybar --reload > /dev/null 2>&1 &
-fi
