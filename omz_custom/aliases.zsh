615c7a342a95f23fc134ffd89fa44b34acac9957
arlohb <arlo@urwick.co.uk>
2022-08-22 21:34:12 +0100

    Cat alias now works for both debian and arch

--- omz_custom/aliases.zsh
+++ omz_custom/aliases.zsh
@@ -1,5 +1,9 @@
 alias ls='exa -la --header --git --icons --no-time'
-alias cat='batcat --theme=Dracula --color=always'
+
+if ! type "bat" > /dev/null; then
+	alias bat='batcat'
+fi
+alias cat='bat --theme=Dracula --color=always'
 
 ghc() { git clone https://github.com/"$@"; }
 alias config='git --git-dir=$HOME/.cfg --work-tree=$HOME'
