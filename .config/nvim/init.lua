798924ec6443d67906b5ee053c0d22347303d732
arlohb <arlo@urwick.co.uk>
2022-09-29 21:22:11 +0100

    Added nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,61 @@
+-- https://mattermost.com/blog/how-to-install-and-set-up-neovim-for-code-editing/
+
+-- Leader
+-- These keybindings need to be defined before the first /
+-- is called; otherwise, it will default to "\"
+vim.g.mapleader = ' '
+-- vim.g.localleader = "\\"
+
+local opt = vim.opt
+
+-- Context
+opt.number = true                -- bool: Show line numbers
+opt.relativenumber = true        -- bool: Show relative line numbers
+opt.scrolloff = 4                -- int:  Min num lines of context
+opt.signcolumn = "yes"           -- str:  Show the sign column
+
+-- Filetypes
+opt.encoding = 'utf8'            -- str:  String encoding to use
+opt.fileencoding = 'utf8'        -- str:  File encoding to use
+
+-- Theme
+opt.syntax = "ON"                -- str:  Allow syntax highlighting
+opt.termguicolors = true         -- bool: If term supports ui color then enable
+
+-- Search
+opt.ignorecase = true            -- bool: Ignore case in search patterns
+opt.smartcase = true             -- bool: Override ignorecase if search contains capitals
+opt.incsearch = true             -- bool: Use incremental search
+opt.hlsearch = false             -- bool: Highlight search matches
+
+-- Whitespace
+opt.expandtab = true             -- bool: Use spaces instead of tabs
+opt.shiftwidth = 4               -- num:  Size of an indent
+opt.softtabstop = 4              -- num:  Number of spaces tabs count for in insert mode
+opt.tabstop = 4                  -- num:  Number of spaces tabs count for
+
+-- Splits
+opt.splitright = true            -- bool: Place new window to right of current one
+opt.splitbelow = true            -- bool: Place new window below the current one
+
+-- Make whichkey show up faster
+opt.timeoutlen = 200
+
+require("plugins")
+
+require("nvim-tree").setup{}
+
+local map = vim.api.nvim_set_keymap
+
+-- Toggle nvim-tree
+map("n", "<leader>n", ":NvimTreeToggle<CR>", {})
+
+vim.g.catppuccin_flavour = "mocha"
+require("catppuccin").setup()
+vim.cmd[[colorscheme catppuccin]]
+
+require("lualine").setup {
+    options = {
+        theme = "catppuccin"
+    }
+}
