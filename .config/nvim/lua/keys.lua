c3d7f3156de2f9f9eb70ba993e8f1d1893e61d66
arlohb <arlo@urwick.co.uk>
2022-10-08 09:43:57 +0100

    Made really nice keybindings for nvim with whichkey

--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -1,21 +1,50 @@
-local map = vim.api.nvim_set_keymap
+local wk = require("which-key")
 
--- Toggle nvim-tree
-map("n", "<leader>n", ":NvimTreeToggle<CR>", {})
+-- Documentation is here:
+-- https://github.com/folke/which-key.nvim
+wk.register({
+    ["<leader>"] = {
+        f = {
+            name = "+file",
+            f = {"<cmd>Telescope find_files<cr>", "Find File"},
+            r = {"<cmd>Telescope oldfiles<cr>", "Recent Files"},
+            s = {"<cmd>Telescope live_grep<cr>", "Search"},
+        },
 
--- Toggle floating terminal
-map("n", "<leader>t", ":FloatermToggle<CR>", {})
--- Quit the terminal while inside it
-map("t", "<Esc>", "<C-\\><C-n><leader>t", {})
+        s = {
+            name = "+search",
+            c = {"<cmd>Telescope commands<cr>", "Commands"},
+        },
 
--- Comment the current line
-map("n", "-", ":Commentary<CR>", {})
--- Comment the current selection
-map("v", "-", ":'<,'>Commentary<CR>", {})
+        w = {
+            name = "+window",
+            h = {"<cmd>vsplit<cr>", "Split Horizontal"},
+            v = {"<cmd>split<cr>", "Split Vertical"},
+            d = {"<cmd>close<cr>", "Close"},
+        },
 
--- Goto definition
-map("n", "gd", ":call CocActionAsync('jumpDefinition')<CR>", {})
--- Show documentation
-map("n", "K", ":call CocActionAsync('doHover')<CR>", {})
+        n = {"<cmd>NvimTreeToggle<cr>", "Toggle NvimTree"},
 
-vim.keymap.set("n", "<leader>a", "<Plug>(coc-snippets-expand-jump)")
+        t = {"<cmd>FloatermToggle<cr>", "Toggle Terminal"},
+    },
+
+    -- Moving between windows
+    ["<C-h>"] = {"<C-w>h", "Go Left"},
+    ["<C-l>"] = {"<C-w>l", "Go Right"},
+    ["<C-j>"] = {"<C-w>j", "Go Down"},
+    ["<C-k>"] = {"<C-w>k", "Go Up"},
+
+    -- Quit the terminal while inside it
+    ["<Esc>"] = {"<cmd>FloatermToggle<cr>", "Quit Terminal", mode = "t"},
+
+    -- Comment the current line
+    ["-"] = {"<cmd>Commentary<CR>", "Comment Line"},
+    -- Comment the current selection
+    ["-"] = {":'<,'>Commentary<CR>", "Comment Line", mode = "v"},
+
+    -- Goto definition
+    gd = {"<cmd>call CocActionAsync('jumpDefinition')<cr>", "Goto Definition"},
+    -- Show documentation
+    K = {"<cmd>call CocActionAsync('doHover')<cr>", "Show Documentation"},
+})
+
