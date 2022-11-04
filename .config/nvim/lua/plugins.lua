a7890315db48874eef50573a35f07ea2549a0f5f
arlohb <arlo@urwick.co.uk>
2022-11-04 11:17:26 +0000

    Added diagnostics to nvim-tree

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -22,7 +22,12 @@ return require('packer').startup(function(use)
         'kyazdani42/nvim-tree.lua',
         requires = 'kyazdani42/nvim-web-devicons',
         config = function()
-            require("nvim-tree").setup{}
+            require("nvim-tree").setup {
+                diagnostics = {
+                    enable = true,
+                    show_on_dirs = true,
+                },
+            }
         end
     }
 
