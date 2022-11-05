33c2d72c1967a79c146f679054987e51ac1b4e85
arlohb <arlo@urwick.co.uk>
2022-11-05 17:51:37 +0000

    Added quiet parameters to boot

--- scripts/install_boot.sh
+++ scripts/install_boot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-efibootmgr --disk /dev/nvme0n1 --part 4 --create --label "Arch Linux" --loader /vmlinuz-linux-zen --unicode 'root=PARTUUID=5cc58d0e-864c-d249-a497-2b6eec7fdc5d rw initrd=\amd-ucode.img initrd=\initramfs-linux-zen.img' --verbose
+efibootmgr --disk /dev/nvme0n1 --part 4 --create --label "Arch Linux" --loader /vmlinuz-linux-zen --unicode 'root=PARTUUID=5cc58d0e-864c-d249-a497-2b6eec7fdc5d rw quiet splash vt.global_cursor_default=0 initrd=\amd-ucode.img initrd=\initramfs-linux-zen.img' --verbose
 
